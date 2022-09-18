dc000b1e9feddb83bf69e915c9cc4a87e2d66229
mithu2001 <guptamithu2019@gmail.com>
2022-09-18 20:08:28 +0530

    committed

---
+++ BasicConcept/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
